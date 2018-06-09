3a7fea7bc0092dff752d86f10b8e8157bde37e8b
Debakar Roy <allinonedibakar@gmail.com>
2018-06-09 17:04:53 +0530

    Added Exercise 11 Asking Questions

---
+++ LPTHW/ex11/ex11.py
@@ -0,0 +1,17 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+'''
+Output:
+
+python ex11.py                              
+How old are you? 21
+How tall are you? 70.8
+How much do you weigh? 121.254
+So, you're 21 old, 70.8 tall and 121.254 heavy.
+'''
---
+++ LPTHW/ex11/ex11_sd_2.py
@@ -0,0 +1,18 @@
+# Other way to use input()
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+'''
+Output:
+
+python ex11_sd_2.py                              
+How old are you? 21
+How tall are you? 70.8
+How much do you weigh? 121.254
+So, you're 21 old, 70.8 tall and 121.254 heavy.
+'''
+
+# P.S.: Did I just did the next exercise while playing with input()
