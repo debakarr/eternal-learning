60fa61666e848bf082b5e879f06deadb3c81ffbc
Debakar Roy <allinonedibakar@gmail.com>
2018-06-10 17:34:50 +0530

    Added Excercise 34 Accessing Elements Of Lists

---
+++ LPTHW/ex34/ex34.py
@@ -0,0 +1,43 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("animals list:", animals)
+
+# 1. The animal at 1.
+print("The animal at 1", animals[1])
+
+# 2. The third (3rd) animal.
+print("The third (3rd) animal:", animals[2])
+
+# 3. The first (1st) animal.
+print("The first (1st) animal:", animals[0])
+
+# 4. The animal at 3.
+print("The animal at 3:", animals[3])
+
+# 5. The fifth (5th) animal.
+print("The fifth (5th) animal:", animals[4])
+
+# 6. The animal at 2.
+print("The animal at 2:", animals[2])
+
+# 7. The sixth (6th) animal.
+print("The sixth (6th) animal:", animals[5])
+
+# 8. The animal at 4.
+print("The animal at 4:", animals[4])
+
+
+'''
+Output:
+
+$ python ex34.py
+animals list: ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+The animal at 1 python3.6
+The third (3rd) animal: peacock
+The first (1st) animal: bear
+The animal at 3: kangaroo
+The fifth (5th) animal: whale
+The animal at 2: peacock
+The sixth (6th) animal: platypus
+The animal at 4: whale
+'''
