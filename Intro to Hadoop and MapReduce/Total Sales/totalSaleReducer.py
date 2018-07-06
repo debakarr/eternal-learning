bbc2ca07788dd21cc2b588d78916261600ed4df6
Debakar Roy <allinonedibakar@gmail.com>
2018-07-06 13:07:51 +0530

    Adder mapper and reducer for Total Sales Quiz

---
+++ Intro to Hadoop and MapReduce/Total Sales/totalSaleReducer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+
+salesTotal = 0
+count = 0
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    _, thisSale = data_mapped
+
+    salesTotal += float(thisSale)
+    count = count + 1
+
+print count, "\t", salesTotal
+
