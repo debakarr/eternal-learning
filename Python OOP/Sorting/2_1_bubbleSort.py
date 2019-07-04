c952741a9996662a89d828c45d3b8d1514d4dbd3
Dibakarroy1997 <debakar.roy@outlook.com>
2019-07-04 12:21:23 +0530

    Adding more TODO Problems

--- Python OOP/Sorting/2_1_bubbleSort.py
+++ Python OOP/Sorting/2_1_bubbleSort.py
@@ -1,1 +1,27 @@
-# TODO
+# Bubble sort
+
+
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        swap = False
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                swap = True
+                print "Swap %d <--> %d" % (arr[i], arr[j])
+                arr[i], arr[j] = arr[j], arr[i]
+
+        if not swap:
+            break
+
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [6, 2, 8, 1, 5, 3, 9, 7]
+    print "\nBefore Sort:", arr 
+    sort = bubbleSort(arr)
+    print "After Sort:", sort
+    arr = [1, 2, 3, 4, 5, 6, 7, 8]
+    print "\nBefore Sort:", arr 
+    sort = bubbleSort(arr)
+    print "After Sort:", sort
